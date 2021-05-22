24d7424f3be238e84bdcf8ae4bcf51a71ef25b26
gurgutadaniel <84647542+gurgutadaniel@users.noreply.github.com>
2021-05-23 02:47:42 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,70 @@
+print("---------Calculator Deficit Caloric-------------")
+print("................................................")
+varsta = int(input("Introdu vârsta: "))
+greutate = int(input("Introdu greutatea în kg: "))
+inaltime = int(input("Introdu înălțimea în cm: "))
+sex = int(input("Sexul: 1.Masculin 2. Feminin "))
+print("Nivelul de activitate fizică:")
+print(" 1. Muncă sedentară și puține alte activități")
+print(" 2. Muncă în picioare sau exerciții ușoare 1-3 zile pe săptămână")
+print(" 3. Activități zilnice moderate sau exerciții 5-6 zile pe săptămână")
+print(" 4. Exerciții intense zi de zi sau muncă fizică intensă plus exerciții 1-3 zile pe săptămână")
+print(" 5. Sportiv de performanță sau muncă fizică intensă plus exerciții de peste 3 ori pe săptămână")
+activitate = int(input("Introdu valoarea: " ))
+rmbb = int(10*greutate+6.25*inaltime-5*varsta+5)
+rmbf = int(10*greutate+6.25*inaltime-5*varsta-161)
+#Bărbat
+print("--------------------------------------------------------")
+if sex == 1 and activitate==1:
+    print("Rata metabolică bazală este:",int(rmbb*1.2),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbb*1.2-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbb*1.2-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbb*1.2-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+elif activitate==2:
+    print("-> Rata metabolică bazală este:",int(rmbb*1.375),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbb*1.375-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbb*1.375-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbb*1.375-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+elif activitate==3:
+    print("-> Rata metabolică bazală este:",int(rmbb*1.55),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbb*1.55-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbb*1.55-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbb*1.55-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+elif activitate==4:
+    print("-> Rata metabolică bazală este:",int(rmbb*1.725),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbb*1.725-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbb*1.725-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbb*1.725-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+elif activitate==5:
+    print("-> Rata metabolică bazală este:",int(rmbb*1.9),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbb*1.9-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbb*1.9-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbb*1.9-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+#Femeie
+if sex == 2 and activitate == 1:
+    print("-> Rata metabolică bazală este:",int(rmbf*1.2),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbf*1.2-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbf*1.2-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbf*1.2-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult  !!")
+elif activitate==2:
+    print("-> Rata metabolică bazală este:",int(rmbf*1.375),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbf*1.375-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbf*1.375-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbf*1.375-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+elif activitate==3:
+    print("-> Rata metabolică bazală este:",int(rmbf*1.55),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbf*1.55-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbf*1.55-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbf*1.55-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+elif activitate==4:
+    print("-> Rata metabolică bazală este:",int(rmbf*1.725),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbf*1.725-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbf*1.725-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbf*1.725-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")
+elif activitate==5:
+    print("-> Rata metabolică bazală este:",int(rmbf*1.9),"Kcal/zi")
+    print("-> Slăbire ușoară 0.25 kg/săpt =",int(rmbf*1.9-250),"Kcal/zi")
+    print("-> Slăbire medie 0.50 kg/săpt =",int(rmbf*1.9-500),"Kcal/zi")
+    print("-> Slăbire extremă 1.00 kg/săpt =",int(rmbf*1.9-1000),"Kcal/zi \n !! Consultă medicul dacă vrei să slăbești 0.5 kg/săpt sau mai mult !!")  
+
+print("--------------------------------------------------------")
